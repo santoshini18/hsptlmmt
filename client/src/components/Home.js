1ec10b90f19210c08bbca9127c7669971daf2ce9
santoshini18 d <santoshini.d18@gmail.com>
2023-04-21 10:56:53 +0000

    Changes made on Fri Apr 21 10:56:53 AM UTC 2023

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -114,7 +114,7 @@ const Home = () => {
           <div className='col mt-5'>
             <h1 className='text-success text-center'><b>ABOUT US </b></h1>
             <h2 className='text-success text-center'><b> What we are and our history</b></h2>
-            <p className='text-success'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, READ MORE
+            <p className='text-success'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting <link>READ MORE</link>
             </p>
            
           </div>
