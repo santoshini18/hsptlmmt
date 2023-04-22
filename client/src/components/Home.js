6d9ae106f6fb5d8ee82342f0965c03e86c59f66d
santoshini18 d <santoshini.d18@gmail.com>
2023-04-22 13:24:42 +0000

    Changes made on Sat Apr 22 01:24:42 PM UTC 2023

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -675,7 +675,7 @@ const Home = () => {
 
 <div className="destination-section">
       <div className='heading'>
-        <h1 className='text-center pt-4'>Destination</h1>
+        <h1 className='text-center pt-4'>Our Doctors</h1>
       </div>
       <div className="container ">
         <div className="row mt-5">
@@ -706,14 +706,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="col card">
-            <img className="card-img-top" src="./image/new-york-animate.svg" alt=" " height={200} />
-            <div className="card-body">
-              <h5 className="card-title">New York</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <Link to='' className="btn btn-primary">Go somewhere</Link>
-            </div>
-          </div>
+         
         </div>
 
 
@@ -731,32 +724,10 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="col card mr-3">
-            <img className="card-img-top" src="./image/london-animate.svg" alt=" " height={200} />
-            <div className="card-body">
-              <h5 className="card-title">London</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <Link to='' className="btn btn-primary">Go somewhere</Link>
-            </div>
-          </div>
+         
 
-          <div className="col card mr-3">
-            <img className="card-img-top" src="./image/japan-animate.svg" alt=" " height={200} />
-            <div className="card-body">
-              <h5 className="card-title">Japan</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <Link to='' className="btn btn-primary">Go somewhere</Link>
-            </div>
-          </div>
+          
 
-          <div className="col card">
-            <img className="card-img-top" src="./image/new-york-animate.svg" alt=" " height={200} />
-            <div className="card-body">
-              <h5 className="card-title">New York</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <Link to='' className="btn btn-primary">Go somewhere</Link>
-            </div>
-          </div>
         </div>
 
 
