e3a40f749054cdac692425aa9f8657c29df8fa97
santoshini18 d <santoshini.d18@gmail.com>
2023-04-22 13:28:15 +0000

    Changes made on Sat Apr 22 01:28:15 PM UTC 2023

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -671,11 +671,11 @@ const Home = () => {
 
          {/*doctors*/}    
 <div>abcdd</div>
+       
 
-
-<div className="destination-section">
+    <div className="destination-section">
       <div className='heading'>
-        <h1 className='text-center pt-4'>Our Doctors</h1>
+        <h1 className='text-center pt-4'>Destination</h1>
       </div>
       <div className="container ">
         <div className="row mt-5">
@@ -706,7 +706,7 @@ const Home = () => {
             </div>
           </div>
 
-         
+          
         </div>
 
 
@@ -724,16 +724,25 @@ const Home = () => {
             </div>
           </div>
 
-         
+          <div className="col card mr-3">
+            <img className="card-img-top" src="./image/london-animate.svg" alt=" " height={200} />
+            <div className="card-body">
+              <h5 className="card-title">London</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <Link to='' className="btn btn-primary">Go somewhere</Link>
+            </div>
+          </div>
 
           
 
+         
         </div>
 
 
       </div>
 
     </div>
+
 
 
 
